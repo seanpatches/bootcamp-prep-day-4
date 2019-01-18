3b6eb767fc2f23c43111d19a56c333d0cb1a2b34
Sean Nelson <smnelsonn@gmail.com>
2019-01-17 19:45:38 -0800

    madegreeting

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 //eslint-disable-next-line
 function greet(){
-    console.log('greet function called');
 
     //get first nameby id
-
+    var firstNameInput = document.getElementById('first-name');
+    
     //get last name by id
+    var lastNameInput = document.getElementById('last-name');
 
     //get values of output
+    var firstName = firstNameInput.value;
+    var lastName = lastNameInput.value;
+
 
     //make greeting
+
+    var greeting = 'Hello, ' + firstName + ' ' + lastName + '!';
 
     //display
 
