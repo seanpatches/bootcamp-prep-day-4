af4fe2246400419196c0396c09bff99649b639df
Sean Nelson <smnelsonn@gmail.com>
2019-01-17 19:23:21 -0800

    prepare to add string

--- main.js
+++ main.js
@@ -1,4 +1,19 @@
 //eslint-disable-next-line
 function greet(){
     console.log('greet function called');
+
+    //get first nameby id
+
+    //get last name by id
+
+    //get values of output
+
+    //make greeting
+
+    //display
+
 }
+
+//todo: validate name inputs
+//todo: clear inputs?
+
